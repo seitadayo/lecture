181597d2124c6332a2b7da26b6612575075c84c4
seitadayo <85237449+seitadayo@users.noreply.github.com>
2021-08-08 12:35:27 +0900

    関数宣言についての勉強

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import "./styles.css";
+/**
+ * const,let等の変数宣言
+ */
+// var vall = "var変数";
+// console.log(vall);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //bar変数は上書き可能
+// vall = "var変数を上書き";
+
+// console.log(vall);
+
+// //var変数は再宣言可能
+// var　vall= "var変数を再宣言";
+// console.log(vall);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数"
+// console.log(val3);
+
+// //const変数は上書き不可
+// // val3 ="const変数の上書き";
+
+// const val3 = "const 変数を再宣言";
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "ジャケぇ",
+//   age: 28,
+// };
+// val4.name ="jal";
+// val4.addres = "hiroshima";
+// console.log(val4);
+
+//constで定義したオブジェクトは配列の変更が可能
+// const val5 = ["dog", "vat"];
+// val5[0] = "bird";
+// val5.push("monky");
+// console.log(val5);
+//reactで使われる多くはconst
